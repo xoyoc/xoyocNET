70cfed8689dc26554dee4e241a1a8249a2d34127
xoyoc <xoyoc_l2@hotmail.com>
2015-03-28 08:25:25 -0600

    Modificacion del menu movil

--- wp-content/themes/XoyocNET/js/menu.js
+++ wp-content/themes/XoyocNET/js/menu.js
@@ -6,17 +6,15 @@ function main(){
 	$('.boton__barra').click(function(){
 		/*$('.menu-movil').toggle();*/
 		if(contador == 1){
-			$('#menu-movil').animate({
+			$('nav').animate({
 				left:'0'
 			});
-			alert("Hola el contador es 1");
 			contador = 0;
 		} else {
 			contador = 1;
-			$('#menu-movil').animate({
+			$('nav').animate({
 				left:'-100%'
 			});
-			alert("Hola el contador es otra cosa");
 		}
 
 	});
