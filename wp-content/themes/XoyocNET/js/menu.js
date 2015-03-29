b527cc1088df0f469ac6a741319ea15b3cd97910
xoyoc <xoyoc_l2@hotmail.com>
2015-03-28 19:47:34 -0600

    Modificacion del menu buena

--- wp-content/themes/XoyocNET/js/menu.js
+++ wp-content/themes/XoyocNET/js/menu.js
@@ -2,9 +2,9 @@ $(document).ready(main);
 
 var contador = 1;
 
-function main(){
+function main (){
 	$('.boton__barra').click(function(){
-		/*$('.menu-movil').toggle();*/
+		// /*$('.menu-movil').toggle();*/
 		if(contador == 1){
 			$('nav').animate({
 				left:'0'
@@ -15,7 +15,30 @@ function main(){
 			$('nav').animate({
 				left:'-100%'
 			});
-		}
+		};
 
 	});
 };
+
+/*$(document).ready(main);
+
+var contador = 1;
+
+function main (){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); Forma Sencilla de aparecer y desaparecer
+		
+		if (contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		};
+		
+	});
+};*/
