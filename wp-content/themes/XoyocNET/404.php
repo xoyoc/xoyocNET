c284a81ea90525eaf5f2595561eff60d19d4983f
ANTONIO XOYOC BECERRA FARIAS <xoyoc_l2@hotmail.com>
2015-04-17 18:30:22 -0500

    Actulizacion de los plung y sistemas

--- wp-content/themes/XoyocNET/404.php
+++
@@ -1,3 +0,0 @@
-<?php get_header(); ?>
-<h1 class="error"> Error 404: la URL no existe...</h1>
-<?php get_footer(); ?>
--- wp-content/themes/XoyocNET/extracto-maqueta.php
+++
@@ -1,8 +0,0 @@
-<article class="entrada">
-	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<?php the_post_thumbnail("thumbnail"); ?>
-	<div class="extracto">
-		<p><?php the_excerpt(); ?></p>
-	</div>
-</article>
-<hr class="separador" />
