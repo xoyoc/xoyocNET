5b516f9cbb31a01ddb412a79685a004e49da13d2
ANTONIO XOYOC BECERRA FARIAS <xoyoc_l2@hotmail.com>
2015-04-28 13:35:58 -0500

    modificacion del menu y functions.php

--- wp-content/themes/XoyocNET/functions.php
+++ wp-content/themes/XoyocNET/functions.php
@@ -2,14 +2,14 @@
 	// IMAGENES DESTACADAS
 	add_theme_support("post-thumbnails");
 	// MENU DE MI PAGINA WEB
-	function register_my_menu(){
+	function register_my_menus(){
 		register_nav_menus(
 			array(
-					'menu-header'=> ___('Menu de encabezado'),
-					'menu-footer'=> ___('Menu de pie de pagina')
+				'menu-header'=> __('Menu de encabezado'),
+				'menu-footer'=> __('Menu de pie de pagina')
 				)
 		 );
 	}
-	add_action('init', 'register_my_menu');
+	add_action('init', 'register_my_menus');
 
 
