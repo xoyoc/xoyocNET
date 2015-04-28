d18a5951f6d7a2bb5b674d2bdd666ac946694d6d
ANTONIO XOYOC BECERRA FARIAS <xoyoc_l2@hotmail.com>
2015-04-28 16:02:00 -0500

    modificacion del menu y functions.php

--- wp-content/themes/XoyocNET/category.php
+++ wp-content/themes/XoyocNET/category.php
@@ -1,15 +1,5 @@
 <?php 
 get_header(); 
-if(have_posts()):
-	while(have_posts()):
-		the_post();
-		get_template_part("extracto-maqueta");
-	endwhile;
-	wp_reset_query();
-else:
-?>
-	<p class="error">No hay informacion solicitada</p>
-<?php 
-endif;
+include (TEMPLATEPATH . '/templates/the_loop.php');
 get_footer();
 ?>
