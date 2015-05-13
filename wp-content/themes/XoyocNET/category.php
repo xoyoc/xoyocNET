154979e451ff10244756564bb0bb3a32d03385e6
ANTONIO XOYOC BECERRA FARIAS <xoyoc_l2@hotmail.com>
2015-05-13 17:40:09 -0500

    Actulizacion de wordpress 4.2.2

--- wp-content/themes/XoyocNET/category.php
+++
@@ -1,8 +0,0 @@
-<?php 
-get_header();
-?>
-<h2><?php bloginfo('name'); ?></h2>
-<?php
-include (TEMPLATEPATH . '/templates/the_loop.php');
-get_footer();
-?>
--- wp-content/themes/XoyocNET/page.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	get_header();
-	the_post();
-?>
-<article class="pagina">
- 	<?php the_content(); ?>
-</article>
-<?php get_footer(); ?>
